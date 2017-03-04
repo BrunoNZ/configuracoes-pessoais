fa78e02b8b7246ba7c7d791b449e9271e789f105
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2017-03-04 00:24:12 -0300

    remove Themes

--- temas/instalar_arc-theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git pull
-
-./autogen.sh --prefix=/usr --disable-transparency && sudo make install
-
--- temas/instalar_moka-icon-theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git pull
-
-bash autogen.sh --prefix=/usr && make && sudo make install
