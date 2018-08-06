2048716beb80f3b7d9262adca922457838a08182
Bruno Nocera Zanette <brunonzanette@gmail.com>
2018-08-06 20:28:00 -0300

    update

--- scripts/atualiza_projetos.sh
+++ scripts/atualiza_projetos.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-projetos="lynx-eletrico lynx-eletrico-api lynx-eletrico-db lynx-energetico"
+projetos="
+	lynx-eletrico
+	lynx-eletrico-api
+	lynx-eletrico-db
+	lynx-energetico
+	simulador
+	hidrosim-mcsearch"
 
 for proj in ${projetos}; do
     echo "#### GIT-PULL: ${proj} ####"
