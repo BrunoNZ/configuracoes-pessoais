5a49a23ef3bb84dfc0651b604dbcb29bc72e1df2
Bruno Nocera Zanette <brunonzanette@gmail.com>
2021-04-02 16:25:59 -0300

    Update

--- scripts/get_yt_mp3s.sh
+++ scripts/get_yt_mp3s.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+if [[ $# != 1 ]]; then
+	echo "Parâmetros inválidos! Use:"
+	echo "$ $0 [ARQUIVO_LISTA]"
+	exit 1
+fi
+
 files=${1}
 
+
 OPTS="--extract-audio --audio-format mp3 --audio-quality 5 --quiet "
+youtubedl="python3 $(dirname ${0})/youtube-dl"
+
+${youtubedl} --update
 
 while read url; do
-	youtube-dl ${OPTS} ${url}
+	${youtubedl} ${OPTS} ${url}
 	if [ $? == 0 ]; then
 		echo "OK: ${url}"
 	else
