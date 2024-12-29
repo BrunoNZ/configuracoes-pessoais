95f6e02863b1a5a5aa438f2d64358982e9b6cabc
Bruno Nocera Zanette <brunonzanette@gmail.com>
2024-12-29 15:44:49 -0300

    Ajustes em MonitorScripts

--- monitor_scripts/switch_monitor_mode.sh
+++ monitor_scripts/switch_monitor_mode.sh
@@ -2,14 +2,7 @@
 INPUT_TYPE="${1}"
 CONFIGS_DIR="$(dirname ${0})/configs"
 
-echo ${INPUT_TYPE}
-case $INPUT_TYPE in
-    pc120hz)      CONFIG_FILE=${CONFIGS_DIR}/pc120hz.xml ;;
-    pc2monitors)  CONFIG_FILE=${CONFIGS_DIR}/pc2monitors.xml ;;
-    tv4k)         CONFIG_FILE=${CONFIGS_DIR}/tv4k.xml ;;
-    tv4k)         CONFIG_FILE=${CONFIGS_DIR}/tv2k.xml ;;
-    *) exit 1 ;;
-esac
+CONFIG_FILE=${CONFIGS_DIR}/${INPUT_TYPE}.xml
 
 if [ ! -f ${CONFIG_FILE} ]; then
     exit 1
